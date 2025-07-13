056a508f49488fc16a7529daaea42894c77182c8
Sibahle Senda <30323491+sibahle81@users.noreply.github.com>
2025-07-13 14:07:22 +0200

    Update encryption-utility.ts

--- Web/RMA-Web-Shell/projects/shared-utilities-lib/src/lib/encryption-utility/encryption-utility.ts
+++ Web/RMA-Web-Shell/projects/shared-utilities-lib/src/lib/encryption-utility/encryption-utility.ts
@@ -1,16 +1,1 @@
-// THIS NEEDS WORK THERE WAS A PERFORMANCE HIT ON THE JS LIBRARY
-//import * as CryptoJS from 'crypto-js';
 
-const SECRET_KEY = 'd7b21d53-4f6a-4c5f-9b8c-9a7d6c8e3b67';
-
-export class EncryptionUtility {
-  static encryptData(data: string): string {
-    return  data; //CryptoJS.AES.encrypt(data, SECRET_KEY).toString();
-  }
-
-  static decryptData(data: string): string {
-    return data;
-    // const bytes = CryptoJS.AES.decrypt(data, SECRET_KEY);
-    // return bytes.toString(CryptoJS.enc.Utf8);
-  }
-}
